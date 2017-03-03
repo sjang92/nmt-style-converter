534edd4b5a4a5880b0527db9a63f9052d55105e1
Se Won Jang <swjang@stanford.edu>
2017-03-03 15:55:39 -0800

    implemented nmt_cell module

---
+++ nmt_cell.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+
+from tensorflow.models.rnn import rnn_cell
+
+class Custom_Cell(object):
+    def __init__(self, size):
+        """
+        This is an empty capsule for our custom cells. Only use when we're trying
+        to define our own RNN cell. Otherwise, specify cell_type="gru" or "lstm"
+        when initializing the NMT_Cell_Generator
+        """
+
+class NMT_Cell_Generator(object):
+    def __init__(self, cell_type, num_layers=1, size):
+        """
+        cell_type = gru, lstm, custom
+        size = number of units in each layer of the model
+        """
+        self.cell_type=cell_type
+        self.num_layers = num_layers
+        self.size = size
+
+    def get_cell(self):
+        if self.cell_type == "gru":
+            cell = rnn_cell.GRUCell(self.size)
+        elif self.cell_type == "lstm":
+            cell = rnn_cell.BasicLSTMCell(self.size)
+        else:
+            cell = Custom_Cell(self.size)
+
+        if num_layers > 1:
+            cell = rnn_cell.MultiRNNCell([cell] * self.num_layers)
+
+        return cell
--- nmt_model.py
+++ nmt_model.py
@@ -1,6 +1,27 @@
 import tensorflow as tf
 
+
 class NMT_Model(object):
 
-    def __init__(self. src_vocab_size, dst_vocab_size, buckets, size, 
-                 num_layers, )
+    def __init__(self, src_vocab_size, dst_vocab_size, buckets, size,
+                 num_layers, max_grad_norm, batch_size, lr, lr_decay, 
+                 use_lstm=False, num_samples=512, forward_only=False):
+
+        """
+        Initialize the NMT Model
+        """
+
+        # since we're dealing with the same language, vocab size should be the same
+        assert src_vocab_size == dst_vocab_size, "vocab size should be the same"
+        self.src_vocab_size = src_vocab_size
+        self.dst_vocab_size = dst_vocab_size
+
+        self.buckets = buckets
+        self.batch_size = batch_size
+        self.num_layers = num_layers
+
+        self.lr = tf.Variable(float(lr), trainable=False)
+
+        # TODO : use sampled softmax?
+
+    def createCell
