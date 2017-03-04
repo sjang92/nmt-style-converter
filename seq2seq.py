952241215b570a44264ef72496ab5bb2668d3eb9
Se Won Jang <swjang@stanford.edu>
2017-03-04 01:17:38 -0800

    writing attention decoder

--- seq2seq.py
+++ seq2seq.py
@@ -1,7 +1,25 @@
 import tensorflow as tf
 
+
+
+def attention_decoder(decoder_inputs, initial_state, attention_states, cell, num_symbols, num_heads=1, output_size=None, output_projection=None, feed_previous=False, dtype=tf.float32, scope=None, initial_state_attention=False, embedding_init=None):
+
+    if output_size is None:
+        output_size = cell.output_size
+    if output_projection is not None:
+        print "output_projection is not none. Check if you want this"
+
+    with tf.variable_scope(scope or 'attention_decoder')
+        embedding = tf.get_variable("embedding", shape=[num_symbols, cell.input_size], initializer=embedding_init)
+
+        # For now, only consider feed_previous = False
+        if not feed_previous:
+            
+
+
+
 def attention_seq2seq(encoder_inputs, decoder_inputs, cell, 
-                      num_encoder_symbols, num_decoder_symbols, encoder_dim,
+                      num_encoder_symbols, num_decoder_symbols, encoder_dim, decoder_dim,
                       num_heads=1, embedding_init=None, output_projection=None, 
                       feed_previous=False, dtype=tf.float32, scope=None, initial_state_attention=None):
     """
@@ -15,6 +33,7 @@ def attention_seq2seq(encoder_inputs, decoder_inputs, cell,
             state: The state of each decoder cell at the final time-step.
                    It is a 2D Tensor of shape [batch_size x cell.state_size].
     """
+
     with tf.variable_scope(scope or 'attention_seq2seq'):
         # Run Encoder first
         encoder_cell = tf.nn.rnn_cell.EmbeddingWrapper(cell, num_encoder_symbols, encoder_dim, embedding_init)
@@ -26,4 +45,5 @@ def attention_seq2seq(encoder_inputs, decoder_inputs, cell,
 
         # Decoding part
         # TODO : Implement Decoding
+        return attention_decoder(decoder_inputs, encoder_state, attention_states, cell, num_decoder_symbols, num_heads=num_heads, output_size=None, output_projection=output_projection, feed_previous=feed_previous, initial_state_attention=initial_state_attention, embedding_init=embedding_init)
 
