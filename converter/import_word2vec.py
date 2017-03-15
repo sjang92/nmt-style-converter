cf0075b078bdbd8de3d1a6166e81b000e1c3edf2
Se Won Jang <swjang@stanford.edu>
2017-03-15 12:22:35 -0700

    refine w2v

--- converter/import_word2vec.py
+++ converter/import_word2vec.py
@@ -7,29 +7,41 @@ model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-nega
 
 print "Loading our vocab file..."
 
+SYMBS = {'_UNK' : 'UNK', '_PAD' : 'PAD', '_EOS': 'EOS', '_GO': 'GO'}
 vocabs = {}
+
+vocabs = []
+indices = []
+counter = 0
 
 with open('./data/all_modern.vocab', 'r') as vocab_file:
     for char in vocab_file:
-        vocabs[char.strip('\n')] = len(vocabs.keys()) # ignore new_line
+        if char in SYMBS:
+            char = SYMBS[char]
+        vocabs.append(char.strip('\n').strip('.'))
+        indices.append(counter)
+        counter += 1
+        #vocabs[char.strip('\n').strip('.')] = len(vocabs.keys()) # ignore new_line
 
     vocab_file.close()
 
 
-embedding_matrix = np.zeros(shape = [len(vocabs.keys()), 300])
+embedding_matrix = np.zeros(shape = [len(vocabs), 300])
 
 unincluded = []
 
 included_count = 0
+counter = 0
 
 # loop through the entire word<->vector map
-for word in vocabs.keys():
+for word in vocabs:
     if word in model:
         included_count += 1
         vec = model[word]
-        embedding_matrix[vocabs[word]] = vec
+        embedding_matrix[indices[counter]] = vec
     else :
         unincluded.append(word)
+    counter += 1
 
 print embedding_matrix.shape
 
